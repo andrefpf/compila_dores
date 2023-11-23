1f05ff77cd77378cfd5771e0fa1e15c2f6ecc04b
André Fernandes <fpf.andre@gmail.com>
2023-11-22 23:30:56 -0300

    implement regex to automata conversion

--- automata.py
+++ automata.py
@@ -1,6 +1,15 @@
 DEAD_STATE_INDEX = -1
 
 
+from dataclasses import dataclass
+
+
+@dataclass
+class State:
+    name: str
+    is_final: bool
+    tag: str = ""
+
 class FiniteAutomata:
     def __init__(
             self, 
@@ -44,6 +53,20 @@ class FiniteAutomata:
         else:
             state = self.states[last_state_index]
             return state.is_final
+
+    def compute(self, origin, symbol):
+        """
+        Executes a single step of computation from a origin state through a symbol, then returns the next state.
+        The symbol & is used to mark epsilon transitions. If the symbol to match in the string is & we look for
+        a transition through "\\&", this way we can handle this symbol with the automata.
+        """
+
+        if symbol == "&":
+            transition = (origin, "\\&")
+        else:
+            transition = (origin, symbol)
+
+        return self.transition_map.get(transition, DEAD_STATE_INDEX)
 
     def _create_transition_map(self, transitions):
         """
--- regex.py
+++ regex.py
@@ -1,7 +1,10 @@
-from automata import FiniteAutomata
+from automata import FiniteAutomata, State
 from grammar import Grammar, Production, EPSILON
 from parser_ll1 import Parser
 
+from collections import defaultdict
+from collections import defaultdict, deque
+from itertools import count
 from treelib import Tree
 
 
@@ -36,6 +39,9 @@ class RegexNode:
                 stack.append(child)
 
         return tree
+
+    def __str__(self) -> str:
+        return str(self.tree_str())
 
 class UnionNode(RegexNode):
     pass
@@ -146,7 +152,7 @@ for i in "qwertyuiopasdfghjklçzxcvbnm QWERTYUIOPASDFGHJKLÇZXCVBNM 0123456789":
     productions.append(prod)
 
 regex_grammar = Grammar(productions)
-syn = Parser(regex_grammar)
+regex_parser = Parser(regex_grammar)
 
 # for i in regex_grammar.first.items():
 #     print(i)
@@ -160,5 +166,176 @@ syn = Parser(regex_grammar)
 #     print(a, "\t", b)
 # print()
 
-tokens = "ab|a(bc)d*"
-s = syn.analyze(tokens)
+def anotate_tree(tree: RegexNode) -> RegexNode:
+    tree = ConcatNode(tree, EndMarkerNode())
+    _recursive_anotate_tree(tree, 0)
+    return tree
+
+
+def calculate_followpos(tree: RegexNode) -> defaultdict[int, set]:
+    followpos = defaultdict(set)
+    _recursive_followpos(tree, followpos)
+    return dict(followpos)
+
+def _recursive_followpos(tree: RegexNode, followpos: defaultdict[int, set]):
+    if tree is None:
+        return
+
+    if isinstance(tree, ClosureNode):
+        for i in tree.lastpos:
+            for j in tree.firstpos:
+                followpos[i].add(j)
+        _recursive_followpos(tree.children[0], followpos)
+
+    elif isinstance(tree, ConcatNode):
+        left_node = tree.children[0]
+        right_node = tree.children[1]
+        for i in left_node.lastpos:
+            for j in right_node.firstpos:
+                followpos[i].add(j)
+        _recursive_followpos(left_node, followpos)
+        _recursive_followpos(right_node, followpos)
+
+    elif isinstance(tree, UnionNode):
+        left_node = tree.children[0]
+        right_node = tree.children[1]
+        _recursive_followpos(left_node, followpos)
+        _recursive_followpos(right_node, followpos)
+
+
+def _recursive_anotate_tree(tree: RegexNode, tag: int) -> int:
+    """
+    Recursive function to calculate firstpos and lastpos for all trees.
+    """
+    if isinstance(tree, EpsilonNode):
+        tree.nullable = True
+        return tag
+
+    if isinstance(tree, (SymbolNode, EndMarkerNode)):
+        tree.firstpos = {tag}
+        tree.lastpos = {tag}
+        tree.nullable = False
+        return tag + 1
+
+    if isinstance(tree, UnionNode):
+        for node in tree.children:
+            tag = _recursive_anotate_tree(node, tag)
+        tree.firstpos = set.union(*[node.firstpos for node in tree.children])
+        tree.lastpos = set.union(*[node.lastpos for node in tree.children])
+        tree.nullable = any([node.nullable for node in tree.children])
+        return tag
+
+    if isinstance(tree, ConcatNode):
+        for node in tree.children:
+            tag = _recursive_anotate_tree(node, tag)
+        
+        left_node = tree.children[0]
+        right_node = tree.children[1]
+
+        if left_node.nullable:
+            tree.firstpos = left_node.firstpos | right_node.firstpos
+        else:
+            tree.firstpos = left_node.firstpos
+
+        if right_node.nullable:
+            tree.lastpos = left_node.lastpos | right_node.lastpos
+        else:
+            tree.lastpos = right_node.lastpos
+
+        tree.nullable = left_node.nullable and right_node.nullable
+        return tag
+
+    if isinstance(tree, ClosureNode):
+        tag = _recursive_anotate_tree(tree.children[0], tag)
+        tree.nullable = True
+        tree.firstpos = tree.children[0].firstpos
+        tree.lastpos = tree.children[0].lastpos
+        return tag
+
+def _get_automata_parameters(*, first_tagset, final_leaf_tag, alphabet, followpos, symbol_tags):
+    states = []
+    transitions = []
+
+    tagset_to_index = defaultdict(count().__next__)  # gives a new index for new elements
+    tagset_queue = deque()
+    tagset_queue.appendleft(first_tagset)
+
+    while tagset_queue:
+        tagset = tagset_queue.pop()
+
+        i = tagset_to_index[tagset]
+        is_final = final_leaf_tag in tagset
+        states.append(State(f"q{i}", is_final))
+
+        for symbol in alphabet:
+            u = frozenset()
+            for i in tagset & symbol_tags[symbol]:
+                u |= followpos[i]
+
+            if u not in tagset_to_index:
+                tagset_queue.appendleft(u)
+
+            transition = (tagset_to_index[tagset], symbol, tagset_to_index[u])
+            transitions.append(transition)
+
+    return states, transitions
+
+
+def get_leafs(tree):
+    if isinstance(tree, SymbolNode):
+        return [tree]
+
+    leafs = []
+    for node in tree.children:
+        leafs.extend(get_leafs(node))
+    return leafs
+
+
+def compiles(expression: str) -> FiniteAutomata:
+    """
+    Converts a regular expression into equivalent Finite Automata.
+    """
+
+    node = regex_parser.analyze(expression)
+    tree = node.syn_tree
+
+    tree = anotate_tree(tree)
+    followpos = calculate_followpos(tree)
+    leafs = get_leafs(tree)
+
+    alphabet = [leaf.symbol for leaf in leafs]
+
+    symbol_tags = defaultdict(set)
+    for leaf in leafs:
+        symbol_tags[leaf.symbol] |= leaf.firstpos
+
+    states, transitions = _get_automata_parameters(
+        first_tagset=frozenset(tree.firstpos),
+        final_leaf_tag=tuple(tree.children[1].firstpos)[0],
+        alphabet=alphabet,
+        followpos=followpos,
+        symbol_tags=symbol_tags,
+    )
+
+    return FiniteAutomata(
+        states=states, transitions=transitions, alphabet=alphabet, initial_state_index=0
+    )
+
+
+
+
+# tokens = "ab|a(bc)d*"
+# s = syn.analyze(tokens)
+
+# print(s.syn_tree)
+
+automata = compiles(r"ab|a(bc)d*")
+
+word = "abcddd"
+result = automata.evaluate(word)
+print(result)
+
+word = "addd"
+result = automata.evaluate(word)
+print(result)
+
